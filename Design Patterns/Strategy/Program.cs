a24d8ca11068214b06435ff5c63f09936e82dac5
Utku Çakar <cakarutku1@hotmail.com>
2025-07-20 23:56:04 +0300

    Strategy desihn pattern work

---
+++ Design Patterns/Strategy/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.CreditCalculatorBase = new After2025CreditCalculator();
+            customerManager.SaveCredit();
+
+            customerManager.CreditCalculatorBase = new Before2025CreditCalculator();
+            customerManager.SaveCredit();
+            Console.ReadLine();
+        }
+    }
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+
+    class Before2025CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated usign before 2025");
+        }
+    }
+
+    class After2025CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated usign after 2025");
+        }
+    }
+
+    class CustomerManager
+    {
+        public CreditCalculatorBase CreditCalculatorBase { get; set; }
+
+        public void SaveCredit()
+        {
+            Console.WriteLine("Customer manager business");
+            CreditCalculatorBase.Calculate();
+        }
+    }
+}
