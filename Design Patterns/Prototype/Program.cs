66a891726488941d3dc74a8eee9b62601ae605db
Utku Çakar <cakarutku1@hotmail.com>
2025-07-20 17:27:37 +0300

    Prototype design work

---
+++ Design Patterns/Prototype/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Utku",
+                LastName = "Çakar",
+                City = "İstanbul"
+            };
+
+            var customer2 = (Customer)customer.Clone();
+            customer2.FirstName = "Umut";
+
+            Console.WriteLine(customer.FirstName);
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Person
+    {
+        public abstract Person Clone();
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+
+    public class Customer : Person
+    {
+        public string City { get; set; }
+
+        // Elimizdeki customer'ı clonelamamızı sağlar
+        public override Person Clone()
+        {
+            return (Person) MemberwiseClone();
+        }
+    }
+
+    public class Employee : Person
+    {
+        public decimal Salary { get; set; }
+
+        // Elimizdeki customer'ı clonelamamızı sağlar
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+}
