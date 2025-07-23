d2b9cbf19cd8f0b6b1ba0296b21d7066984b0a48
Utku Çakar <cakarutku1@hotmail.com>
2025-07-23 10:01:59 +0300

    added create own list in generics work

---
+++ Generics2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Generics2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generics ile kendi list'imizi oluşturma
+
+            MyList<string> myList = new MyList<string>();
+            myList.Add("a");
+            myList.Add("b");
+            myList.Add("c");
+
+            foreach (var list in myList.Items)
+            {
+                Console.WriteLine(list);
+            }
+            Console.WriteLine(myList.Length);
+            Console.ReadLine();
+        }
+    }
+}
